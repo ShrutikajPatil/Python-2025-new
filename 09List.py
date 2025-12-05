37bd202e9843335b2136933326487e896ccea947
Shrutika Patil <shrutikapatil890@gmail.com>
2025-12-05 17:08:11 +0530

    adding new file

---
+++ 09List.py
@@ -0,0 +1,39 @@
+# List is in puthon is collection of items/element that are ordered changable and allow duplicates values
+# it is most versatile data structure that are allow to store multiple values in single variable.
+# it can contain comma separated values in square bracckets
+# eg. list=['apple','banana',897,'a','b','c']
+# you can access elements in a list by referring to theirr index.python used zero-based indexing ,meaning the first elemet has an index of 0
+
+lst=['apple','banana',897,'a','b','c']
+
+print(lst[3])
+print(lst[0])
+print(lst[1])
+print(lst[-1])
+print(type(lst))
+
+
+lst2=[1,2,3,[40,45,56],560,700]
+
+print(lst2[3][0])
+print(lst2[-2])
+
+lst3=[10,20,30,40,50,60,70]
+
+print(lst3[1:5])
+print(lst3[:5])
+print(lst3[-2:])
+print(lst3[::-1])
+print(lst3[::-2])
+
+
+
+
+
+
+# access list using indexing
+
+ 
+
+
+
